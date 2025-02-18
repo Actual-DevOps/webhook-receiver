ac8a81db68c61849c103cf8f8f3eb7c4a090278c
Boris Shestov <shestov1989@mail.ru>
2025-02-18 15:05:39 +0300

    feat: Add flag to conf and help via cobra pkg

--- Dockerfile
+++ Dockerfile
@@ -18,12 +18,15 @@ RUN apk --no-cache add ca-certificates \
     && update-ca-certificates \
     && addgroup -g 9999 app \
     && adduser -s /dev/false -u 9999 -D -G app app \
+    && mkdir -p /app/config \
     && chown -R app:app /app \
     && rm -rf /var/cache/apk/*
 
 COPY --from=builder /app/webhook-receiver /app/webhook-receiver
 
 EXPOSE 8081
+
+VOLUME ["/app/config"]
 
 USER app
 
--- main.go
+++ main.go
@@ -4,13 +4,41 @@ import (
 	"fmt"
 	"log/slog"
 	"net/http"
+	"os"
 
 	"github.com/elbars/webhook_receiver/internal/config"
 	"github.com/elbars/webhook_receiver/internal/handlers"
+	"github.com/spf13/cobra"
 )
 
+func wrapHelpFunctionWithExit(cmd *cobra.Command) {
+	helpFunc := cmd.HelpFunc()
+	cmd.SetHelpFunc(func(cmd *cobra.Command, args []string) {
+		helpFunc(cmd, args)
+		os.Exit(0)
+	})
+}
+
 func main() {
-	cfg, err := config.LoadConfig("config.yaml")
+	rootCmd := &cobra.Command{
+		Use:   "go run main.go",
+		Short: "Path to config",
+		Run:   func(cmd *cobra.Command, args []string) {},
+	}
+	wrapHelpFunctionWithExit(rootCmd)
+
+	defaultConfigPath := "config/config.yaml"
+
+	var configPath string
+
+	rootCmd.Flags().StringVarP(&configPath, "config", "c", defaultConfigPath, "Сonfig path")
+
+	if err := rootCmd.Execute(); err != nil {
+		slog.Error(err.Error())
+	}
+
+	cfg, err := config.LoadConfig(configPath)
+
 	if err != nil {
 		slog.Error("Failed to load config: " + err.Error())
 	}
